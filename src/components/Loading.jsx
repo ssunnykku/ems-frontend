54f2297d16cb7d221c940a4d7053f5b931ede802
HelloNuri <HelloNuri.dvl@gmail.com>
2024-07-16 14:21:54 +0900

    loading

---
+++ src/components/Loading.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import Palette from '../styles/Palette';
+import PropTypes from 'prop-types';
+import '../styles/loading.css';
+
+const Loading = () => {
+  return (
+      <LoadingWrapper id='loading'></LoadingWrapper>
+  );
+};
+
+const LoadingWrapper = styled.div`
+`;
+export default Loading;
+  
