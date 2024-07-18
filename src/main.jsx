ac3840e821a8c429b2b02a9ca07aea86eff217ae
ssunnykku <108388578+ssunnykku@users.noreply.github.com>
2024-07-18 15:49:01 +0900

    feat: axios 테스트중

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { CookiesProvider } from 'react-cookie';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <CookiesProvider defaultSetOptions={{ path: '/' }}>
     <App />
-  </React.StrictMode>
+  </CookiesProvider>
+  // </React.StrictMode>
 );
